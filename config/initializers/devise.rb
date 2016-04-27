ce1e72c8ccf3cdaf8b4ba8798a3ececc1a077ff0
Mai Anh Tuan <perhuman@gmail.com>
2016-04-27 21:43:23 +0700

    [f] New Login UI

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -263,3 +263,8 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 end
+
+# https://github.com/plataformatec/devise/wiki/How-To:-Create-custom-layouts
+Rails.application.config.to_prepare do
+  Devise::SessionsController.layout 'admin'
+end
