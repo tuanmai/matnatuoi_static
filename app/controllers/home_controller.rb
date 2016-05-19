fb950a5318db7432197d0fc39cc47ea036794b43
Mai Anh Tuan <perhuman@gmail.com>
2016-05-19 19:54:40 +0700

    [f] Basic homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     set_meta_tags site: 'Mặt nạ tươi - Xinh Lắm cơ Í', title: 'Trang Chủ'
+    @current_week = Week.last
+    @previous_weeks = Week.where.not(id: @current_week.id).last(3)
   end
 end
