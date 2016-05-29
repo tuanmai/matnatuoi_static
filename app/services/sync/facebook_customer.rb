c5aad967b33a9fb0f83c7b266a063bcfc9e6d721
Mai Anh Tuan <perhuman@gmail.com>
2016-05-29 15:39:38 +0700

    [b] Remove combo from sync customer data. Refactor code

--- app/services/sync/facebook_customer.rb
+++ app/services/sync/facebook_customer.rb
@@ -1,24 +1,27 @@
 module Sync
   class FacebookCustomer
     def call
-      get_customer_data_from_notes
+      sync_customers_from_notes
     end
 
     private
-
-    def get_customer_data_from_notes
+    def sync_customers_from_notes
       Customer.update_from_google_drive
       FbPageApi.admin_notes.collection.map do |note|
-        customer = find_or_create_customer(note['user'])
-        new_attributes =  extract_customer_attributes(note['body'])
-        if new_attributes
-          customer.attributes = new_attributes
-          customer.save
-        else
-          customer.add_note(note['body'])
-        end
-        customer
+        sync_customer_from_node(note)
       end
+    end
+
+    def sync_customer_from_node(note)
+      customer = find_or_create_customer(note['user'])
+      new_attributes =  extract_customer_attributes(note['body'])
+      if new_attributes
+        customer.attributes = new_attributes
+        customer.save
+      else
+        customer.add_note(note['body'])
+      end
+      customer
     end
 
     def find_or_create_customer(facebook_user)
@@ -29,9 +32,9 @@ module Sync
     end
 
     def extract_customer_attributes(fb_note)
-      skin_type, allergy, prefer, combo, phone_number, address, *customer_notes = *fb_note.split(';')
+      skin_type, allergy, prefer, phone_number, address, *customer_notes = *fb_note.split(';')
       note = customer_notes.join(';')
-      if [skin_type, allergy, prefer, combo, phone_number, address].compact.size == 6
+      if [skin_type, allergy, prefer, phone_number, address].compact.size == 6
         {
           skin_type: skin_type,
           allergy: allergy,
