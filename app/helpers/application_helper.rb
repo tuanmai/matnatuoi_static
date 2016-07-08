564805c4896ceddcb0dfacbef2066f50cf4ecf41
Mai Anh Tuan <perhuman@gmail.com>
2016-07-08 20:25:09 +0800

    beauty UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def new_sidebar_section_for(title, &block)
-    content_tag(:div, class:'mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp portfolio-card') do
+    content_tag(:div, class:'mdl-cell mdl-cell--12-col mdl-card sidebar-card') do
       content_tag(:div, class: 'mdl-card__title') do
-        content_tag(:h3, class: 'mdl-card__title-text') { title }
+        content_tag(:span, class: '') { title }
       end +
       content_tag(:div, class: 'mdl-card__supporting-text') do
         capture(&block)
